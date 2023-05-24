855b16e752ab7f096a3d000b23a3bf4e2eea3b5d
John Leon <jleonprofessional@gmail.com>
2023-05-24 12:01:36 -0500

    Change ascii to utf-8

---
+++ Test/StringHelperTest.py
@@ -0,0 +1,18 @@
+import unittest
+from src.StringHelper import encode, decode
+
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_encode(self):
+        try:
+            encode('Foo').decode('utf-8')
+        except UnicodeDecodeError:
+            self.fail('Should not have thrown exception')
+
+
+    def test_decode(self):
+        try:
+            decode('Foo'.encode('utf-8'))
+        except UnicodeDecodeError:
+            self.fail('Should not have thrown exception')
